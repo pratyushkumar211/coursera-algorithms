c910d2527efb95f8bf4565dd68199f26abc746c2
Pratyush Kumar <pratyushkumar211@gmail.com>
2018-06-14 19:56:03 -0500

    add initial code structure for assignment I

---
+++ part-I/assignment-1/PercolationStats.java
@@ -0,0 +1,10 @@
+public class PercolationStats {
+	public PercolationStats(int n, int trials)
+	public double mean()
+	public double stddev()
+	public double confidenceLo()
+	public double confidenceHi()
+
+	public static void main(String[] args)
+
+}
